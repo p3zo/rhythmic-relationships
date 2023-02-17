09b5ecddc3b2f32e727d6c5e93eedd1abc5eb3bb
p3zo <p3zo@users.noreply.github.com>
2023-02-17 00:38:38 -0800

    Update sampling method; write y pattern

--- scripts/inference_0a.py
+++ scripts/inference_0a.py
@@ -26,21 +26,16 @@ if __name__ == "__main__":
     x = x_in.view(x_in.shape[0], config["model"]["x_dim"])
     y = y_in.view(y_in.shape[0], config["model"]["y_dim"])
 
-    # Encode the x with a random y
-    random_y = torch.randn_like(y)
-    mu, sigma = model.encode(x, random_y)
+    # Sample a random point in the latent space
+    z = torch.randn(1, config["model"]["z_dim"])
 
-    # Sample the latent space
-    epsilon = torch.randn_like(sigma)
-    z = mu + sigma * epsilon
-
-    # Reconstruct x using the sample
-    recon_x = model.decode(z, random_y)
-    recon_in = recon_x.view(x_in.shape).detach().numpy()
+    # Generate a new x
+    recon_x = model.decode(z, y)
+    recon = recon_x.view(x_in.shape).detach().numpy()
 
     # Apply an onset threshold
     # TODO: adjust the threshold if the input is not binarized
-    out = np.ma.masked_array(recon_in, mask=(recon_in < 0.2), fill_value=0).filled()[0]
+    out = np.ma.masked_array(recon, mask=(recon < 0.05), fill_value=0).filled()[0]
 
     # Assign the onsets a reasonable MIDI velocity
     out[out.nonzero()] = 80
@@ -48,7 +43,7 @@ if __name__ == "__main__":
 
     # Save the output
     inference_dir = os.path.join(
-        config["dataset"]["dataset_dir"], f"inference_{get_model_name(config)}"
+        config["dataset"]["dataset_dir"], "inference", get_model_name(config)
     )
     if not os.path.isdir(inference_dir):
         os.makedirs(inference_dir)
@@ -57,7 +52,7 @@ if __name__ == "__main__":
         # Write the output roll as both MIDI and image
         write_image_from_roll(out, os.path.join(inference_dir, "prediction.png"))
         write_midi_from_roll(
-            out.T,
+            out,
             os.path.join(inference_dir, "prediction.mid"),
             resolution=24,
         )
@@ -67,11 +62,16 @@ if __name__ == "__main__":
         in_roll[in_roll.nonzero()] = 80
         write_image_from_roll(in_roll, os.path.join(inference_dir, "input.png"))
         write_midi_from_roll(
-            in_roll.T, os.path.join(inference_dir, "input.mid"), resolution=24
+            in_roll, os.path.join(inference_dir, "input.mid"), resolution=24
         )
     elif config["dataset"]["repr_1"] == "pattern":
         # Write the output pattern as MIDI
-        write_midi_from_pattern(out.T, os.path.join(inference_dir, "prediction.mid"))
+        write_midi_from_pattern(out, os.path.join(inference_dir, "prediction.mid"))
 
         # Write the input pattern as MIDI
-        write_midi_from_pattern(x_in.T, os.path.join(inference_dir, "input.mid"))
+        write_midi_from_pattern(x_in, os.path.join(inference_dir, "input.mid"))
+
+    if config["dataset"]["repr_2"] == "pattern":
+        # Write the y pattern as MIDI
+        in_y = y_in[0].numpy().astype(np.int8)
+        write_midi_from_pattern(in_y, os.path.join(inference_dir, "label.mid"))
