5ede1e8615fa74293293831a30c83ce018760242
p3zo <p3zo@users.noreply.github.com>
2023-03-02 17:23:18 -0800

    Update part tests

--- tests/test_parts.py
+++ tests/test_parts.py
@@ -1,4 +1,4 @@
-from scripts.prepare_data import get_part_from_program
+from rhythmic_complements.parts import get_part_from_program, get_part_pairs
 
 
 def test_get_part_from_program():
@@ -7,3 +7,12 @@ def test_get_part_from_program():
     assert get_part_from_program(8) == "Chromatic Percussion"
     assert get_part_from_program(10) == "Chromatic Percussion"
     assert get_part_from_program(127) == "Sound Effect"
+
+
+def test_get_part_pairs():
+    parts = ["Brass", "Piano", "Organ"]
+    assert get_part_pairs(parts) == [
+        ("Piano", "Brass"),
+        ("Piano", "Organ"),
+        ("Organ", "Brass"),
+    ]
