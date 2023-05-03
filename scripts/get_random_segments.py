15bfaef918c96f05e8f6042fb71e0aabf790a82a
p3zo <p3zo@users.noreply.github.com>
2023-05-03 01:32:55 -0700

    Update script for getting random segments

--- scripts/get_random_segments.py
+++ scripts/get_random_segments.py
@@ -1,37 +1,87 @@
 """Select segments with at least n parts from a dataset at random."""
 
+import argparse
 import os
 
 import numpy as np
 from rhythmic_relationships import DATASETS_DIR
 from rhythmic_relationships.data import load_dataset_annotations
-from rhythmic_relationships.io import get_pmid_segment, load_midi_file
+from rhythmic_relationships.io import get_pmid_segment_reprs, load_midi_file
 
-INPUT_DATA_DIR = "../input/lmd_clean"
 
-dataset_name = "lmdc_250_1bar_4res"
-representation = "roll"
-n_parts_min = 2
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="lmdc_1000_2bar_4res",
+        help="Name of the dataset to make plots for.",
+    )
+    parser.add_argument(
+        "--midi_dir",
+        type=str,
+        default="../input/lmd_clean",
+        help="Name of the directory from which to load MIDI data.",
+    )
+    parser.add_argument(
+        "--filename",
+        type=str,
+        default="Cocciante/Cervo a primavera",
+        help="The name of the MIDI file to load from.",
+    )
+    parser.add_argument(
+        "--n_samples",
+        type=int,
+        default=5,
+        help="The number of samples to select.",
+    )
+    parser.add_argument(
+        "--n_parts_min",
+        type=int,
+        default=2,
+        help="The minimum number of parts to include in a segment.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="output/midi",
+        help="The directory to save the output MIDI files to.",
+    )
 
-# Load dataset annotations
-df = load_dataset_annotations(os.path.join(DATASETS_DIR, dataset_name))
-df = df.sample(frac=1)  # shuffle
+    args = parser.parse_args()
 
-# Choose a random segment with more than `n_parts_min` parts
-n_parts_by_seg = df.groupby(["segment_id", "filepath"]).apply(len)
+    dataset_name = args.dataset
+    filename = args.filename
+    midi_dir = args.midi_dir
+    n_samples = args.n_samples
+    n_parts_min = args.n_parts_min
+    output_dir = args.output_dir
 
-output_dir = os.path.join("output", "midi")
-for n in range(5):
-    seg_id, fp = np.random.choice(n_parts_by_seg[n_parts_by_seg >= n_parts_min].index)
-    seg_df = df.loc[(df.filepath == fp) & (df.segment_id == seg_id)]
+    # Load dataset annotations
+    df = load_dataset_annotations(os.path.join(DATASETS_DIR, dataset_name))
+    df = df.sample(frac=1)  # shuffle
 
-    track_name = os.path.splitext("/".join(fp.split("/")[-2:]))[0]
-    pmid = load_midi_file(os.path.join(INPUT_DATA_DIR, f"{track_name}.mid"))
+    # Choose a random segment with more than `n_parts_min` parts
+    n_parts_by_seg = df.groupby(["segment_id", "filepath"]).apply(len)
 
-    parts = seg_df.part_id.values.tolist()
-    pmid_slice = get_pmid_segment(pmid, segment_num=seg_id, seg_size=2, parts=parts)
+    for n in range(n_samples):
+        seg_id, fp = np.random.choice(
+            n_parts_by_seg[n_parts_by_seg >= n_parts_min].index
+        )
+        seg_df = df.loc[(df.filepath == fp) & (df.segment_id == seg_id)]
 
-    file_seg_id = f"{os.path.splitext(os.path.basename(fp))[0]}_{seg_id}"
-    print(f"Selected segment {file_seg_id}\n\tParts: {parts}\n")
+        track_name = os.path.splitext("/".join(fp.split("/")[-2:]))[0]
+        pmid = load_midi_file(os.path.join(midi_dir, f"{track_name}.mid"))
 
-    pmid_slice.write(os.path.join(output_dir, f"{file_seg_id}.mid"))
+        parts = seg_df.part_id.values.tolist()
+        (
+            pmid_roll,
+            pmid_onset_roll,
+            pmid_onset_roll_3o,
+            hits_list,
+        ) = get_pmid_segment_reprs(pmid, seg_id, parts=parts)
+
+        file_seg_id = f"{os.path.splitext(os.path.basename(fp))[0]}_{seg_id}"
+        print(f"Selected segment {file_seg_id}\n\tParts: {parts}\n")
+
+        pmid_onset_roll.write(os.path.join(output_dir, f"{file_seg_id}.mid"))
