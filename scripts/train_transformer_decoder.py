8ac6e6eeb0408c4cee23e6f17edb634650d7a590
p3zo <p3zo@users.noreply.github.com>
2023-05-08 18:27:46 -0700

    Update training config

--- scripts/train_transformer_decoder.py
+++ scripts/train_transformer_decoder.py
@@ -1,6 +1,8 @@
+import argparse
 import torch
 import yaml
 from model_utils import get_loss_fn, get_model_name, load_config, save_model
+from rhythmic_relationships import DATASETS_DIR
 from rhythmic_relationships.data import PartDatasetSequential
 from rhythmic_relationships.model import TransformerDecoder
 from rhythmic_relationships.train import train_transformer_decoder
@@ -10,6 +12,17 @@ DEVICE = torch.device("mps" if torch.backends.mps.is_built() else "cpu")
 CONFIG_FILEPATH = "transformer_decoder_config.yml"
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--datasets_dir",
+        type=str,
+        default=DATASETS_DIR,
+        help="Path to the dir containing the dataset.",
+    )
+    args = parser.parse_args()
+
+    datasets_dir = args.datasets_dir
+
     print(f"{DEVICE=}")
 
     config = load_config(CONFIG_FILEPATH)
@@ -17,7 +30,7 @@ if __name__ == "__main__":
 
     torch.manual_seed(config["seed"])
 
-    dataset = PartDatasetSequential(**config["dataset"])
+    dataset = PartDatasetSequential(**config["dataset"], datasets_dir=datasets_dir)
     splits = config["splits"]
     train_data, val_data, test_data = random_split(dataset, list(splits.values()))
     print(f"{splits=}: {len(train_data)}, {len(val_data)}, {len(test_data)}")
