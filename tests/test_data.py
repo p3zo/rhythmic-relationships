ee6390c2ed0fc35c95bee06096a09c1e9705bcfa
p3zo <p3zo@users.noreply.github.com>
2023-05-22 22:30:48 -0700

    Test get_pair_sequences; add more TransformerEncoderDecoder training functions

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,27 +1,42 @@
+import numpy as np
 import pytest
 import torch
 from rhythmtoolbox import DESCRIPTOR_NAMES
-from rhythmic_relationships.data import PartDataset, PartPairDataset
+from rhythmic_relationships.data import (
+    PAD_TOKEN,
+    PartDataset,
+    PartPairDataset,
+    get_pair_sequences,
+)
 from torch.utils.data import DataLoader
 
-dataset_name = "babyslakh_20_1bar_4res"
+DATASET_NAME = "babyslakh_20_1bar_4res"
 batch_size = 1
-part_1 = "Drums"
-part_2 = "Bass"
+PART_1 = "Drums"
+PART_2 = "Bass"
 
 N_DESCRIPTORS = len(DESCRIPTOR_NAMES)
 
 
+def test_get_pair_sequences():
+    p1 = np.array([1, 3, 4])
+    p2 = np.array([1, 2, 2])
+    context_len = 3
+    X, Y = get_pair_sequences(p1, p2, context_len)
+    assert X == [[PAD_TOKEN, PAD_TOKEN, 1], [PAD_TOKEN, 1, 3], [1, 3, 4]]
+    assert Y == [[PAD_TOKEN, PAD_TOKEN, 1], [PAD_TOKEN, 1, 2], [1, 2, 2]]
+
+
 def test_PartDataset():
     with pytest.raises(Exception):
-        PartDataset(dataset_name, part_1, "incorrect-descriptor-name")
+        PartDataset(DATASET_NAME, PART_1, "incorrect-descriptor-name")
 
-    data = PartDataset(dataset_name, part_1, "descriptors")
+    data = PartDataset(DATASET_NAME, PART_1, "descriptors")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x = next(iter(loader))
     assert x.size() == torch.Size([batch_size, N_DESCRIPTORS])
 
-    data = PartDataset(dataset_name, part_1, "roll")
+    data = PartDataset(DATASET_NAME, PART_1, "roll")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x = next(iter(loader))
     assert x.size() == torch.Size([batch_size, 16, 128])
@@ -30,70 +45,70 @@ def test_PartDataset():
 def test_PartPairDatasets():
     with pytest.raises(Exception):
         PartPairDataset(
-            dataset_name, part_1, part_2, "desc", "incorrect-descriptor-name"
+            DATASET_NAME, PART_1, PART_2, "desc", "incorrect-descriptor-name"
         )
 
-    data = PartPairDataset(dataset_name, part_1, part_2, "roll", "roll")
+    data = PartPairDataset(DATASET_NAME, PART_1, PART_2, "roll", "roll")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x, y = next(iter(loader))
     assert x.size() == torch.Size([batch_size, 16, 128])
     assert y.size() == torch.Size([batch_size, 16, 128])
 
-    data = PartPairDataset(dataset_name, part_1, part_2, "descriptors", "descriptors")
+    data = PartPairDataset(DATASET_NAME, PART_1, PART_2, "descriptors", "descriptors")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x, y = next(iter(loader))
     assert x.size() == torch.Size([batch_size, N_DESCRIPTORS])
     assert y.size() == torch.Size([batch_size, N_DESCRIPTORS])
 
-    data = PartPairDataset(dataset_name, part_1, part_2, "roll", "descriptors")
+    data = PartPairDataset(DATASET_NAME, PART_1, PART_2, "roll", "descriptors")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x, y = next(iter(loader))
     assert x.size() == torch.Size([batch_size, 16, 128])
     assert y.size() == torch.Size([batch_size, N_DESCRIPTORS])
 
-    data = PartPairDataset(dataset_name, part_1, part_2, "descriptors", "roll")
+    data = PartPairDataset(DATASET_NAME, PART_1, PART_2, "descriptors", "roll")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x, y = next(iter(loader))
     assert x.size() == torch.Size([batch_size, N_DESCRIPTORS])
     assert y.size() == torch.Size([batch_size, 16, 128])
 
-    data = PartPairDataset(dataset_name, part_1, part_2, "hits", "hits")
+    data = PartPairDataset(DATASET_NAME, PART_1, PART_2, "hits", "hits")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x, y = next(iter(loader))
     assert x.size() == torch.Size([batch_size, 16])
     assert y.size() == torch.Size([batch_size, 16])
 
-    data = PartPairDataset(dataset_name, part_1, part_2, "hits", "roll")
+    data = PartPairDataset(DATASET_NAME, PART_1, PART_2, "hits", "roll")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x, y = next(iter(loader))
     assert x.size() == torch.Size([batch_size, 16])
     assert y.size() == torch.Size([batch_size, 16, 128])
 
-    data = PartPairDataset(dataset_name, part_1, part_2, "descriptors", "hits")
+    data = PartPairDataset(DATASET_NAME, PART_1, PART_2, "descriptors", "hits")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x, y = next(iter(loader))
     assert x.size() == torch.Size([batch_size, N_DESCRIPTORS])
     assert y.size() == torch.Size([batch_size, 16])
 
-    data = PartPairDataset(dataset_name, part_1, part_2, "pattern", "pattern")
+    data = PartPairDataset(DATASET_NAME, PART_1, PART_2, "pattern", "pattern")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x, y = next(iter(loader))
     assert x.size() == torch.Size([batch_size, 16])
     assert y.size() == torch.Size([batch_size, 16])
 
-    data = PartPairDataset(dataset_name, part_1, part_2, "roll", "pattern")
+    data = PartPairDataset(DATASET_NAME, PART_1, PART_2, "roll", "pattern")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x, y = next(iter(loader))
     assert x.size() == torch.Size([batch_size, 16, 128])
     assert y.size() == torch.Size([batch_size, 16])
 
-    data = PartPairDataset(dataset_name, part_1, part_2, "pattern", "chroma")
+    data = PartPairDataset(DATASET_NAME, PART_1, PART_2, "pattern", "chroma")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x, y = next(iter(loader))
     assert x.size() == torch.Size([batch_size, 16])
     assert y.size() == torch.Size([batch_size, 16, 12])
 
-    data = PartPairDataset(dataset_name, part_1, part_2, "chroma", "roll")
+    data = PartPairDataset(DATASET_NAME, PART_1, PART_2, "chroma", "roll")
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     x, y = next(iter(loader))
     assert x.size() == torch.Size([batch_size, 16, 12])
